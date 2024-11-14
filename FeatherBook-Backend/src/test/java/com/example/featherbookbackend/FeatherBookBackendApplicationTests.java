1410b65053ff3c62b7cd81a689d079d38ba330d8
cristianipa7 <cristianir007@gmail.com>
2024-11-13 22:45:21 -0500

    feat(setup): initialize project with Spring Boot dependencies

---
+++ FeatherBook-Backend/src/test/java/com/example/featherbookbackend/FeatherBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.featherbookbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatherBookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
